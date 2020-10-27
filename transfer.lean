02e32ea9fa5d8024dcf58d70f7f75a40f0fa9806
KoenKahlman <k.kahlman@student.vu.nl>
2020-10-27 10:58:03 +0100

    transfer1 works on all examples

--- transfer.lean
+++ transfer.lean
@@ -73,6 +73,10 @@ do
     ) (tactic.fail "oeps")
   <||> tactic.fail "exit repeat"),
 
+  trace_state,
+
+  -- skip
+
   tactic.interactive.finish [] finishing,
 
   skip
@@ -102,120 +106,122 @@ end transfer
 
 --name naar tactic.expr = get_local in tactic.lean
 
-theorem test (a b : Type) (ha : a) (hab : a → b) : b := 
-begin
-  --apply_at ha using hab,
-  apply_at hab ha using id,
-  assumption,
-  -- assumption, --finding ha_1
-end  
+-- theorem test (a b : Type) (ha : a) (hab : a → b) : b := 
+-- begin
+--   --apply_at ha using hab,
+--   apply_at hab ha using id,
+--   assumption,
+--   -- assumption, --finding ha_1
+-- end  
 
 
-def N : Type := sorry
-def ordern : N → N → Prop := sorry
-def nto : N → nat := sorry
-def nof : nat → N := sorry
+-- def N : Type := sorry
+-- def ordern : N → N → Prop := sorry
+-- def nto : N → nat := sorry
+-- def nof : nat → N := sorry
 
-axiom nto_surj : function.surjective nto
-axiom nof_surj : function.surjective nof
-axiom le_ordern_nof : ∀ m n : nat, m <= n → ordern (nof m) (nof n)
-axiom le_ordern_nof_iff : ∀ m n : nat,  m <= n ↔ ordern (nof m) (nof n) --temp hack
-axiom ordern_nof_le : ∀ m n : nat, ordern (nof m) (nof n) → m <= n
+-- axiom nto_surj : function.surjective nto
+-- axiom nof_surj : function.surjective nof
+-- axiom le_ordern_nof : ∀ m n : nat, m <= n → ordern (nof m) (nof n)
+-- -- axiom le_ordern_nof_iff : ∀ m n : nat,  m <= n ↔ ordern (nof m) (nof n) --temp hack
+-- axiom ordern_nof_le : ∀ m n : nat, ordern (nof m) (nof n) → m <= n
 
-theorem transitiveorder_nat : ∀ x y z : nat, x <= y → y <= z → x <= z := sorry
+-- theorem transitiveorder_nat : ∀ x y z : nat, x <= y → y <= z → x <= z := sorry
 
-theorem transitiveorder_N : ∀ x y z : N, ordern x y → ordern y z → ordern x z :=
-begin
-  -- intros,
-  -- rw [← classical.some_spec (nof_surj x)] at *,
-  -- rw [← classical.some_spec (nof_surj y)] at *,
-  -- rw [← classical.some_spec (nof_surj z)] at *,
+-- theorem transitiveorder_N : ∀ x y z : N, ordern x y → ordern y z → ordern x z :=
+-- begin
+--   -- intros,
+--   -- rw [← classical.some_spec (nof_surj x)] at *,
+--   -- rw [← classical.some_spec (nof_surj y)] at *,
+--   -- rw [← classical.some_spec (nof_surj z)] at *,
 
-  -- apply_at a using ordern_nof_le _ _,
-  -- apply_at a_1 using ordern_nof_le _ _,
+--   -- apply_at a using ordern_nof_le _ _,
+--   -- apply_at a_1 using ordern_nof_le _ _,
 
-  -- apply le_ordern_nof,
-  -- finish using [transitiveorder_nat],
+--   -- apply le_ordern_nof,
+--   -- finish using [transitiveorder_nat],
 
-  transfer.transfer1 ``(nof_surj) [``(ordern_nof_le _ _)] [``(le_ordern_nof), ``(transitiveorder_nat)],
+--   transfer.transfer1 ``(nof_surj) [``(ordern_nof_le _ _)] [``(le_ordern_nof), ``(transitiveorder_nat)],
 
-  --``(le_ordern_nof_iff)
+--   --``(le_ordern_nof_iff)
 
-  --have b := ordern_nof_le _ _ a,
-  --have b_1 := ordern_nof_le _ _ a_1,
+--   --have b := ordern_nof_le _ _ a,
+--   --have b_1 := ordern_nof_le _ _ a_1,
   
-  --apply le_ordern_nof,
-  --finish using transitiveorder_nat,
-end
+--   --apply le_ordern_nof,
+--   --finish using transitiveorder_nat,
+-- end
 
 
 
 
 
--- EXAMPLE 2: transfer the theorem that odd+odd=even from z to z mod 2 and back.
-namespace example2
+-- -- EXAMPLE 2: transfer the theorem that odd+odd=even from z to z mod 2 and back.
+-- namespace example2
 
-definition even (n : int) : Prop := n % 2 = 0
+-- definition even (n : int) : Prop := n % 2 = 0
 
--- a custom type Z/2Z, with operation add and predicate even
-inductive Z2 : Type
-| zero : Z2
-| one : Z2
+-- -- a custom type Z/2Z, with operation add and predicate even
+-- inductive Z2 : Type
+-- | zero : Z2
+-- | one : Z2
 
-open Z2
+-- open Z2
 
-def Z2.add : Z2 → Z2 → Z2
-| zero y := y
-| one zero := one 
-| one one := zero
+-- def Z2.add : Z2 → Z2 → Z2
+-- | zero y := y
+-- | one zero := one 
+-- | one one := zero
 
-inductive Z2.even : Z2 → Prop
-| zeroiseven : Z2.even zero
+-- inductive Z2.even : Z2 → Prop
+-- | zeroiseven : Z2.even zero
 
--- mapping from int to Z2 (no inverse mapping)
-def ztoz2 : int → Z2 := λ n, if n % 2 = 0 then zero else one
+-- -- mapping from int to Z2 (no inverse mapping)
+-- def ztoz2 : int → Z2 := λ n, if n % 2 = 0 then zero else one
 
--- axioms needed for transfer:
--- mapping is surjective
-axiom surjectivemap : function.surjective ztoz2
--- mapping respects add and even (there must be transfer axioms for every operation and predicate in the theorem)
-axiom transfer_add : ∀ m n : int, (ztoz2 m).add (ztoz2 n) = ztoz2(m + n)
-axiom eventoz2 : ∀ n : int, even n → Z2.even (ztoz2 n)
-axiom evenfromz2 : ∀ n : int, Z2.even (ztoz2 n) → even n  -- specific wording of the axioms makes the proof go nicely
+-- -- axioms needed for transfer:
+-- -- mapping is surjective
+-- axiom surjectivemap : function.surjective ztoz2
+-- -- mapping respects add and even (there must be transfer axioms for every operation and predicate in the theorem)
+-- axiom transfer_add : ∀ m n : int, (ztoz2 m).add (ztoz2 n) = ztoz2(m + n)
+-- axiom transfer_add' : ∀ m n : int, ztoz2(m + n) = (ztoz2 m).add (ztoz2 n) -- >:(
 
--- theorem about adding two odd integers, now we want the same result in Z2
-theorem thetheoremforint : ∀ m n : int, ¬ even m → ¬ even n → even (m + n) := sorry
+-- axiom eventoz2 : ∀ n : int, even n → Z2.even (ztoz2 n)
+-- axiom evenfromz2 : ∀ n : int, Z2.even (ztoz2 n) → even n  -- specific wording of the axioms makes the proof go nicely
 
-theorem thetheoremforZ2 : ∀ x y : Z2, ¬ Z2.even x → ¬ Z2.even y → Z2.even (Z2.add x y) := 
-begin
-  intros,
-  transfer.transfer1 ``(surjectivemap) [``(transfer_add), ``(eventoz2)] [``(eventoz2), ``(thetheoremforint)],
-  
-  --apply met not 
+-- -- theorem about adding two odd integers, now we want the same result in Z2
+-- theorem thetheoremforint : ∀ m n : int, ¬ even m → ¬ even n → even (m + n) := sorry
 
-  -- intros,
-  -- rw [← classical.some_spec (surjectivemap x)] at *,
-  -- rw [← classical.some_spec (surjectivemap y)] at *,
-  rw transfer_add,
-  apply eventoz2,
-  apply thetheoremforint;
-  finish using [eventoz2]
-end
-
--- the other direction can also be shown
-theorem thetheoremforint' : ∀ x y : int, ¬ even x → ¬ even y → even (x + y) := 
-begin
-  intros,
-  apply evenfromz2,
-  rw← transfer_add,
-  apply thetheoremforZ2;
-  finish using [evenfromz2]
-end
-
-end example2
+-- theorem thetheoremforZ2 : ∀ x y : Z2, ¬ Z2.even x → ¬ Z2.even y → Z2.even (Z2.add x y) := 
+-- begin
+--   intros,
+--   --rw [← classical.some_spec (surjectivemap x)] at *,
+--   --rw [← classical.some_spec (surjectivemap y)] at *,
+--   --rw transfer_add,
+--   transfer.transfer1 ``(surjectivemap) [``(transfer_add'), ``(eventoz2)] [``(thetheoremforint), ``(eventoz2)],
 
 
+--   --rw transfer_add, THIS WORKS MANUALLY, transfer needs the reverse rewrite though...
 
+--   -- TODO apply met not 
 
+--   -- intros,
+--   -- rw [← classical.some_spec (surjectivemap x)] at *,
+--   -- rw [← classical.some_spec (surjectivemap y)] at *,
+--   -- rw transfer_add,
+--   -- apply eventoz2,
+--   -- apply thetheoremforint;
+--   -- finish using [eventoz2]
+-- end
 
-end transfer
+-- -- the other direction can also be shown
+-- theorem thetheoremforint' : ∀ x y : int, ¬ even x → ¬ even y → even (x + y) := 
+-- begin
+--   intros,
+--   apply evenfromz2,
+--   rw← transfer_add,
+--   apply thetheoremforZ2;
+--   finish using [evenfromz2]
+-- end
+
+-- end example2
